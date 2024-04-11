066421dcbd13423e9259a2365efd1c1ecf03a675
Nikita <fedosovnikita007@gmail.com>
2024-04-11 19:15:52 +0300

    Review part 1
    Я случайно удалил свой проект и делал его заново. На всю жизнь запомню.

---
+++ appModules/rating/index.js
@@ -0,0 +1,10 @@
+const { updateRating, createRating } = require('./calculations');
+const config = require('./config');
+const makeRatingFile = require('./rating-file');
+
+module.exports = {
+    config,
+    makeRatingFile,
+    updateRating,
+    createRating
+}
