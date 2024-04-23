8786ab98e28cc8ca2838e7e962c01dbbc5baff2d
Nikita <fedosovnikita007@gmail.com>
2024-04-23 19:04:30 +0300

    It's work!
    I am happy!

--- middlewares/games.js
+++ middlewares/games.js
@@ -1,4 +1,5 @@
-const { readData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
+const { readData, writeData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
+
 
 const getAllGames = async (req, res, next) => {
     // Читаем список игр из файла
@@ -15,6 +16,67 @@ const getAllGames = async (req, res, next) => {
     next();
 }
 
+const checkIsTitleInArray = (req, _, next) => {
+    req.isNew = !Boolean(req.games.find(item => item.title === req.body.title));
+    next();
+}
+
+const updateGamesArray = async (req, res, next) => {
+    if (req.isNew) {
+        // https://www.icloud.com/keynote/01fnJkCVOjSabEfozpz6cSjfw объект с данными о новой игре
+        const inArray = req.games.map(item => Number(item.id));
+        let maximalId;
+        if (inArray.length > 0) {
+          maximalId = Math.max(...inArray);
+        } else {
+          maximalId = 0;
+        }
+        req.updatedObject = {
+          id: maximalId + 1,
+          title: req.body.title,
+          image: req.body.image,
+          link: req.body.link,
+          description: req.body.description
+        };
+        // Добавляем данные о новой игре в список с другими играми
+        req.games = [...req.games, req.updatedObject];
+        next();
+    } else {
+        res.status(400);
+        res.send({ status: "error", message: "Игра с таким именем уже есть." });
+        return
+    }}
+
+
+const updateGamesFile = async (req, res, next) => {
+    await writeData("./data/games.json", req.games);
+    next();
+}
+
+const findGamesById = async (req, res, next) => {
+    const id = Number(req.params.id);
+    next();
+
+  // Найдём игру, которую хотят удалить, в общем массиве с играми по id
+  req.game = req.games.find((item) => item.id === id);
+  next();
+}
+
+const deleteGame = async (req, res, next) => {
+    // Найдём индекс удаляемой игры в общем массиве игр
+    const id = req.games.id;
+    const index = req.games.findIndex((item) => item.id === req.games.id);
+
+    // Удалим из массива игр игру
+    req.games.splice(index, 1);
+
+  next();
+}
 module.exports = {
-    getAllGames
+    getAllGames,
+    checkIsTitleInArray,
+    updateGamesArray,
+    updateGamesFile,
+    findGamesById,
+    deleteGame
 }
--- routes/games.js
+++ routes/games.js
@@ -2,12 +2,12 @@
 const gamesRouter = require("express").Router(); // Создали роутер
 const {
   sendAllGames,
-  deleteGame,
-  addGameController,
+  sendUpdatedGames
+
 } = require("../controllers/games");
-const { getAllGames } = require("../middlewares/games");
+const { getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, findGamesById, deleteGame} = require("../middlewares/games");
 
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController);
+gamesRouter.delete("/games/:id", getAllGames, findGamesById, deleteGame, updateGamesFile, sendUpdatedGames);
+gamesRouter.post("/games", getAllGames, checkIsTitleInArray,updateGamesArray, updateGamesFile, sendUpdatedGames);
 module.exports = gamesRouter;
