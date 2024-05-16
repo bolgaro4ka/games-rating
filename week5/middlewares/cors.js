fd956ed8a2d42bf7a6b0708c52ad5904b00e8924
Nikita <fedosovnikita007@gmail.com>
2024-05-16 19:06:27 +0300

    Корсики

--- week5/middlewares/cors.js
+++ week5/middlewares/cors.js
@@ -1,17 +1,14 @@
 const allowedCors = [
-  "https://practicum.yandex.ru",
-  "https://students-projects.ru",
-  "http://localhost:3000",
-  "http://localhost:3001",
+  "https://front-bolgaro4ka.nomoredomainswork.ru"
 ];
 function cors(req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    // Если это наш друг
-    res.header('Access-Control-Allow-Origin', '*'); // Открываем доступ всем
+    res.header("Access-Control-Allow-Origin", origin);
   }
-
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
 
