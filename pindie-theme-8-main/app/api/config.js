d040286b38b78ecdf9854e8c9942aea7582863b4
Nikita <fedosovnikita007@gmail.com>
2024-05-16 19:51:25 +0300

    Base

--- pindie-theme-8-main/app/api/config.js
+++ pindie-theme-8-main/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://front-bolgaro4ka.nomoredomainswork.ru';
+export const BASE_URL = 'https://bolgaro4ka.nomoredomainswork.ru';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
