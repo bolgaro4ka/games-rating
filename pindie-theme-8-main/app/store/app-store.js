93444a527adbccba79114dbffed56f046f2c1572
Nikita <fedosovnikita007@gmail.com>
2024-05-28 16:10:29 +0300

    tyertyynyt\r yb f hopegf hdfgh d

--- pindie-theme-8-main/app/store/app-store.js
+++ pindie-theme-8-main/app/store/app-store.js
@@ -1,14 +1,13 @@
 import { create } from 'zustand';
 import { getJWT, setJWT, removeJWT, getMe } from '../api/api-utils';
 import { endpoints } from '../api/config';
-import { isResponseOk } from '../api/api-utils';
 
 export const useStore = create((set) => ({
     isAuth: false,
     user: null,
     token: null,
     login: (user, token) => {
-       set({ isAuth: true, user: { ...user, id: user._id }, token: jwt }); 
+        set({ isAuth: true, user, token });
         setJWT(token);
     },
     logout: () => {
@@ -19,8 +18,9 @@ export const useStore = create((set) => ({
         const jwt = getJWT();
         if (jwt) {
             const user = await getMe(endpoints.me, jwt);
-            if (isResponseOk(user)) {
+            if (user) {
             set({ isAuth: true, user: { ...user, id: user._id }, token: jwt }); 
+
               setJWT(jwt);
             } else {
               set({ isAuth: false, user: null, token: null });
